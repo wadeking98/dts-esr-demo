d17b4747ca10792130fe0837b30837d28173b6f3
Emiliano Suñé <emiliano.sune@gmail.com>
2020-05-03 16:44:34 -0700

    Convert OrgBookSearch component to typescript
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- web/src/components/OrgBookSearch.vue
+++ web/src/components/OrgBookSearch.vue
@@ -1,3 +1,5 @@
+<style lang="scss"></style>
+
 <template>
   <v-autocomplete
     label="Orgbook Search"
@@ -10,7 +12,7 @@
     :rules="rules"
     :items="items"
     :loading="isLoading"
-    :search-input.sync="search"
+    :search-input.sync="searchInput"
     v-on:change="change"
     clearable
     hide-no-data
@@ -19,79 +21,71 @@
   ></v-autocomplete>
 </template>
 
-<script>
-import Vue from "vue";
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import { Configuration } from "../models/appConfig";
 
-export default {
-  name: "OrgBookSearch",
-  props: {
-    fieldModel: String,
-    fieldRules: Array
-  },
-  data() {
-    return {
-      isLoading: false,
-      entries: [],
-      search: null,
-      model: this.fieldModel,
-      rules: this.fieldRules
-    };
-  },
-  computed: {
-    items() {
-      return this.entries.map(entry => {
-        // there will only ever be 1 result in the names array
-        return Object.assign({
-          text: entry.names[0].text,
-          value: entry.names[0].text
-        });
-      });
-    },
-    apiURL() {
-      if (Vue.prototype.$config) {
-        const config = Vue.prototype.$config;
-        return config.orgbook.endpoint;
-      } else {
-        throw new Error("Configuration object is missing.");
-      }
-    }
-  },
-  methods: {
-    change: function(value) {
-      this.$emit(
-        "update:field-model",
-        // For this use, want to emit just the text
-        typeof value === "object" && value !== null ? value.text : value
-      );
-    }
-  },
-  watch: {
-    search(val) {
-      // Minimum search length is 1 character
-      if (!val || val.length < 1) return;
+@Component({})
+export default class OrgBookSearch extends Vue {
+  @Prop() fieldModel!: string;
+  @Prop() fieldRules!: Function[];
 
-      // A search has already been started
-      if (this.isLoading) return;
+  private appConfig!: Configuration;
+  private isLoading = false;
+  private entries = [];
+  private searchInput = "";
+  private model!: string;
+  private rules!: Function[];
 
-      this.isLoading = true;
+  created() {
+    this.appConfig = this.$store.getters[
+      "configuration/getConfiguration"
+    ] as Configuration;
 
-      // Lazily load results
-      fetch(
-        `https://orgbook.gov.bc.ca/api/v2/search/autocomplete?q=${encodeURIComponent(
-          val
-        )}`
-      )
-        .then(res => res.json())
-        .then(res => {
-          this.count = res.results.length;
-          this.entries = res.results;
-        })
-        .catch(err => {
-          // eslint-disable-next-line
-          console.log(err);
-        })
-        .finally(() => (this.isLoading = false));
-    }
+    this.model = this.fieldModel;
+    this.rules = this.fieldRules;
   }
-};
+
+  get items() {
+    return this.entries.map((entry: any) => {
+      // there will only ever be 1 result in the names array
+      return Object.assign({
+        text: entry.names[0].text,
+        value: entry.names[0].text
+      });
+    });
+  }
+
+  change(value: any) {
+    this.$emit(
+      "update:field-model",
+      // For this use, want to emit just the text
+      typeof value === "object" && value !== null ? value.text : value
+    );
+  }
+
+  @Watch("searchInput")
+  onSearchInputChanged(val: string) {
+    // Minimum search length is 1 character
+    if (!val || val.length < 1) return;
+
+    // A search has already been started
+    if (this.isLoading) return;
+
+    this.isLoading = true;
+
+    const orgbookEndpoint = this.appConfig.app.orgBook.endpoint;
+
+    // Lazily load results
+    fetch(`${orgbookEndpoint}/search/autocomplete?q=${encodeURIComponent(val)}`)
+      .then(res => res.json())
+      .then((res: any) => {
+        this.entries = res.results;
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => (this.isLoading = false));
+  }
+}
 </script>
